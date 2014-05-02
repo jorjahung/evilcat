a6df1f87f4be7ba995c0239a10a9be64f007397c
Jorja Hung <jorjahung@gmail.com>
2014-05-02 15:39:04 +0100

    Started project EvilCat

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Evilcat::Application.config.secret_key_base = '695ce649580a00ca0ab224e163f42210e851501cb9ff6aeb2b11734a583dc3383c62b2cb8c13c833162c288a3f6f5ae5b48103c676d73fd1224f31ddc0b14b3f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Evilcat::Application.config.session_store :cookie_store, key: '_evilcat_session'
